0b4e14276e7a15456921e09be5fd2c462be7f421
sanjukta-ch <sanjukta.chanda0513@gmail.com>
2025-09-13 12:24:39 +0530

    Website
    Wrote first few lines of streamlit. To run the website type streamlit run Web.py in your command line.

---
+++ Website/Web.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+# Set the title of the application
+st.title("My First Streamlit App")
+
+# Add a header
+st.header("Interactive Data Display")
+
+# Add some introductory text
+st.write("This app demonstrates how to display data and interact with widgets.")
+st.write("hello")
